8fc33d64461bb055c84e445f4b9bc22f708682d4
shoaib <shobekhan@yahoo.com>
2018-10-14 21:40:03 +0200

    error message changes

--- Model/Cart.php
+++ Model/Cart.php
@@ -16,7 +16,7 @@ class Cart extends \Magento\Checkout\Model\Cart
     public function addProduct($productInfo, $requestInfo = null)
     {
         if ($productInfo->getIsWithMembership() && !$this->_customerSession->isLoggedIn()) {
-            throw new LocalizedException(__('You are not logged in'));
+            throw new LocalizedException(__('In order to buy this product, you need to be a registered and logged in member.'));
         }
         parent::addProduct($productInfo, $requestInfo);
         return $this;
